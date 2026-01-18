270a93f7547dae93d59025e553899df5e4074665
Ryoengineer112358 <ryoengineer112358@gmail.com>
2026-01-18 16:25:42 +0900

    first commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,46 @@
+  type Product = {
+    id: string
+    name: string
+    imageUrl?: string
+    price: number
+    quantity: number
+  }
+  type Paging = {
+    totalCount: number
+    totalPage: number
+    pageSize: number
+    pageNumber: number
+  }
+  type ProductsResponse = {
+    page: {
+      items: Product[]
+      paging: Paging
+    }
+  }
+
+const ProductsPage = async () => {
+
+  const response = await fetch('http://localhost:8082/api/products?likeProductName=&pageSize=10&pageNumber=1&sortCondition=PRICE_ASC');
+
+  if(!response.ok) {
+    return <div>データの取得に失敗しました</div>
+  }
+
+  const products: ProductsResponse = await response.json();
+
+  return (
+  <>
+    <h1 className="text-2xl font-bold">商品一覧</h1>
+    
+    <ul className="list-disc pl-5">
+      {products.page.items.map((product) => (
+        <li key={product.id}>
+          {product.name} - {product.price}円
+        </li>
+      ))}
+    </ul>
+  </>
+)
+}
+
+export default ProductsPage;
